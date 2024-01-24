48ef6723ff820652b2802a4ecc9bce37e22b1a91
srisailambayya12 <157579464+srisailambayya12@users.noreply.github.com>
2024-01-24 15:45:47 +0530

    Swagger changes

--- cd-rates-app/src/main/java/com/cdrates/CdRatesAppApplication.java
+++ cd-rates-app/src/main/java/com/cdrates/CdRatesAppApplication.java
@@ -3,7 +3,28 @@ package com.cdrates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Certificate of Deposit Rates REST API Documentation",
+				description = "CD Rates REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "test",
+						email = "test@example.com",
+						url = "http://localhost:8080/swagger-ui/index.html"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Certificate of Deposits REST API Documentation",
+				url = "http://localhost:8080/swagger-ui/index.html"
+		)
+)
 public class CdRatesAppApplication {
 
 	public static void main(String[] args) {
--- cd-rates-app/src/main/java/com/cdrates/controller/CdRatesController.java
+++ cd-rates-app/src/main/java/com/cdrates/controller/CdRatesController.java
@@ -12,6 +12,12 @@ import com.cdrates.exp.InvalidZipCodeException;
 import com.cdrates.models.CdRateChart;
 import com.cdrates.service.CdRatesService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping("/v3")
 public class CdRatesController {
@@ -19,6 +25,10 @@ public class CdRatesController {
 	@Autowired
 	private CdRatesService cdRatesService;
 
+	@Operation(
+            summary = "Fetch CdRates for customer REST API",
+            description = "REST API to fetch cdrates for customer"
+    )
 	@GetMapping("/customer/cdRateChart/{zipCode}")
 	public List<CdRateChart> getActiveCdRates(@PathVariable String zipCode) {
 
@@ -30,6 +40,10 @@ public class CdRatesController {
 
 	}
 
+	@Operation(
+            summary = "Fetch Cd History Rates for customer REST API",
+            description = "REST API to fetch Cd History Rates for customer"
+    )
 	@GetMapping("/customer/cdHistoryRateChart/{zipCode}")
 	public List<CdRateChart> getCdHistoryRateChart(@PathVariable String zipCode) {
 
@@ -40,6 +54,10 @@ public class CdRatesController {
 		}
 	}
 
+	@Operation(
+			  summary = "Fetch Cd Rates for Manager REST API",
+	          description = "REST API to fetch CdRates for Manager"
+    )
 	@GetMapping(value = "/manager/cdRateChart/{zipCode}")
 	public List<CdRateChart> getActiveManagerCDRates(@PathVariable("zipCode") String zipCode) {
 		if (zipCode.length() == 5) {
@@ -49,6 +67,10 @@ public class CdRatesController {
 		}
 	}
 
+	@Operation(
+			  summary = "Fetch Cd History Rates for Manager REST API",
+	          description = "REST API to fetch Cd History Rates for Manager"
+    )
 	@GetMapping(value = "/manager/cdHistoryRateChart/{zipCode}")
 	public List<CdRateChart> getActiveManagerHistoryCDRates(@PathVariable("zipCode") String zipCode) {
 		if (zipCode.length() == 5) {
